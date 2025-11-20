d5c8d810d9032c4a9ff71168902b519ff43c416c
oscar <oscar.caasmontes@gmail.com>
2025-11-20 12:07:51 +0100

    ajuste rotacion

--- src/components/Radar.jsx
+++ src/components/Radar.jsx
@@ -109,7 +109,7 @@ export default function Radar({
   }, [distance, nav, next, startDynamicBeep]);
 
   // Cálculo de la rotación
-  const ARROW_OFFSET = 220;
+  const ARROW_OFFSET = 240;
   const rotation = heading == null ? 0 : bearing - heading + ARROW_OFFSET;
 
   // Mensaje de distancia para niños (cualitativo/numérico)
